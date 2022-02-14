cc679b542ac4f54fa9903b5500b3a2c9d9d1ef31
Cesar Guibo <cesarguibo0@gmail.com>
2022-02-14 15:55:30 -0300

    replotted graphs

--- exp/format_data.py
+++ exp/format_data.py
@@ -1,5 +1,6 @@
 import os
 import re
+from functools import reduce
 
 RAW_FILES_DIR = './raw_data'
 FORMATED_FILES_DIR = './formated_data'
@@ -10,8 +11,7 @@ if __name__ == '__main__':
         print(f'processing {file_name}')
         with open(f'{RAW_FILES_DIR}/{file_name}', 'r') as in_file:
             header_pattern = r'[A-Za-z_\s]+[A-Za-z_,0-9\s]+\n[^a-zA-Z_]+'
-            csvs = [csv.strip('\n ') for csv in re.findall(header_pattern, in_file.read())]
-            #print([csv[:100] for csv in csvs])
+            csvs = [re.sub('Tmr Svc', 'TmrSvc', csv.strip('\n ')) for csv in re.findall(header_pattern, in_file.read())]
         for i, csv in enumerate(csvs):
             print(f'\twriting {file_name}_{i}')
             with open(f'{FORMATED_FILES_DIR}/{file_name[:-4]}_{i}.csv', 'w') as out_file:
